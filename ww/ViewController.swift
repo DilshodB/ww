cb07c6b20be357359c57a375dc8a4ce51b72d248
DilshodB <nnepke99@gmai.com>
2023-02-15 22:17:09 +0500

    Initial Commit

---
+++ ww/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ww
+//
+//  Created by 1 1 on 15/02/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
